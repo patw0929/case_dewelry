24a84e6b0f5b8e47382088561663ff85e44cfd67
Carlos <chang.abin@gmail.com>
2013-08-31 01:27:06 +0800

    產品內容頁完成

---
+++ js/jewelry_product_fade.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.slideshow')
+	.cycle({
+		fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+		speed:  1500,  //調整輪播秒數
+		
+		pager:  '#nav',
+		pagerAnchorBuilder: function(idx, slide) { 
+        // return selector string for existing anchor
+        return '#nav li:eq(' + idx + ') a'; 
+    }
+	});
+	
+	
+	$('#slider1').tinycarousel({ display: 3 });
+	
+});
