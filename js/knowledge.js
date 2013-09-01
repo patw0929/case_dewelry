a3d7aa0f4749b41af8ef59ce40fa0ec0bf23fc67
patw <patw.hih@gmail.com>
2013-09-01 09:02:06 +0800

    增加 knowledge 單元的回到頂端按鈕

--- js/knowledge.js
+++ js/knowledge.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-	  $("div.middle_container").removeClass("container-fluid");
+$(function () {
+
+    $("div.middle_container").removeClass("container-fluid");
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() > 100) {
+            $("#gotop").fadeIn();
+        } else {
+            $("#gotop").fadeOut();
+        }
+
+    });
+
+    $("#gotop").on('click', function () {
+
+        $("html, body").animate({ scrollTop: 0 }, 900);
+
+    });
+
 });
