8dc864f162154b2eed2761e299ab94942fd9e438
patw <patw.hi@gmail.com>
2013-09-04 18:46:23 +0800

    刪除不用的檔案 (CSS Sprites 原圖、無用 js)

--- js/jewelry.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	//在進入jewelry頁面時，設定滑鼠移過item時，不用再換圖
-	$("#item_icon_jewelry").attr("src", "./images/icon_4_jewelry_hover.png");
-	$("li.item_jewelry a").removeAttr("onmouseover").removeAttr("onmouseout");
-});
